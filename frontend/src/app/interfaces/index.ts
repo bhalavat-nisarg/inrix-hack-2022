b7915ee6d49f357d51bd4d50bd8d6bb95916d817
JAGDISH0008 <jagadeesh26597@gmail.com>
2022-11-12 20:24:23 -0800

    feature: added the cluster data and integrated with map.

---
+++ frontend/src/app/interfaces/index.ts
@@ -0,0 +1,1 @@
+export * from "./osmplace.interface";
---
+++ frontend/src/app/interfaces/osmplace.interface.ts
@@ -0,0 +1,12 @@
+export interface OSMPlace {
+  place_id: number;
+  licence: string;
+  osm_type: string;
+  osm_id: number;
+  lat: number;
+  lon: number;
+  display_name: string;
+  class: string;
+  type: number;
+  importance: number;
+}
